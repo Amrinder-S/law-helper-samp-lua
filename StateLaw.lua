4436afdcff5a55b6d57ada1468f72f71e731d6b6
Amrinder Singh <80534489+Amrinder-S@users.noreply.github.com>
2022-05-05 12:43:17 +0530

    Added text wrap
    looks nicer now

--- StateLaw.lua
+++ StateLaw.lua
@@ -17,7 +17,7 @@ function main()
         wait(0)
         imgui.Process = overlay.v
         if overlay.v then imgui.Process = true else imgui.Process = false end
-		if sampIsChatInputActive() and string.len(sampGetChatInputText()) > 2 and string.find(sampGetChatInputText(), "/su%s+%w+%s+(.+)") then --and not string.find(sampGetChatInputText(), '(.+)/') and string.find(sampGetChatInputText(), '/su%s+%w+%s+(.+)') 
+        if sampIsChatInputActive() and string.len(sampGetChatInputText()) > 2 and string.find(sampGetChatInputText(), "/su%s+%w+%s+(.+)") then --and not string.find(sampGetChatInputText(), '(.+)/') and string.find(sampGetChatInputText(), '/su%s+%w+%s+(.+)') 
 			overlay.v = true
 		else
 			overlay.v = false
@@ -27,24 +27,27 @@ end
 
 function imgui.OnDrawFrame()
     if overlay.v then
-	if TypedLaw then
-		lawText = sampGetChatInputText():match("/su%s+%w+%s+(.+)") --Yes im pro
-		local in1 = sampGetInputInfoPtr()
-		local in1 = getStructElement(in1, 0x8, 4)
-		local in2 = getStructElement(in1, 0x8, 4)
-		local in3 = getStructElement(in1, 0xC, 4)
-		local posY = in3 + 200
-		local posX = in2
-		--local sw, sh = getScreenResolution() Was made to get screen res, might be useful later
-		imgui.SetNextWindowPos(imgui.ImVec2(posX,posY),imgui.Cond.Always,imgui.ImVec2(0.5,0.5))
-		imgui.Begin("Sus", overlay.v, imgui.WindowFlags.NoResize + imgui.WindowFlags.NoCollapse + imgui.WindowFlags.NoTitleBar)
-		imgui.Text(u8"State Law Helper.")
-		imgui.Text(lawText)
-		imgui.Text(stateLaw(lawText))
-		imgui.TextColored(imgui.ImVec4(1,1,0,256), u8"---------------------------------------------------------------------------------")
-		imgui.TextColored(imgui.ImVec4(1,1,1,256), u8"State Law.")
-		imgui.End()
-	end
+		if TypedLaw then
+			lawText = sampGetChatInputText():match("/su%s+%w+%s+(.+)") --Yes im pro
+            local in1 = sampGetInputInfoPtr()
+            local in1 = getStructElement(in1, 0x8, 4)
+            local in2 = getStructElement(in1, 0x8, 4)
+            local in3 = getStructElement(in1, 0xC, 4)
+            local posY = in3 + 200
+            local posX = in2
+			--local sw, sh = getScreenResolution()-- Was made to get screen res, might be useful later
+			imgui.SetNextWindowPos(imgui.ImVec2(posX,posY),imgui.Cond.Always,imgui.ImVec2(0.5,0.5))
+            imgui.SetNextWindowSize(imgui.ImVec2(800,300))
+			imgui.Begin("Sus", overlay.v, imgui.WindowFlags.NoResize + imgui.WindowFlags.NoCollapse + imgui.WindowFlags.NoTitleBar)
+            imgui.PushTextWrapPos(0.0)
+			imgui.Text(u8"State Law Helper.")
+            imgui.TextColored(imgui.ImVec4(1,1,0,256), u8"---------------------------------------------------------------------------------")
+			imgui.Text(lawText)
+			imgui.Text(stateLaw(lawText))
+			imgui.TextColored(imgui.ImVec4(1,1,0,256), u8"---------------------------------------------------------------------------------")
+			imgui.TextColored(imgui.ImVec4(1,1,1,256), u8"Version 1.1")
+			imgui.End()
+		end
     end
 end
 
@@ -162,10 +165,10 @@ local something = {
 }
 
 function stateLaw(arg)
-    local toreturn = "No such law found." --Default in case there's no such law, to avoid script crash.
+    local toreturn = " " --Default in case there's no such law, to avoid script crash.
         for key, value in ipairs(something) do
             if string.find(string.lower(value[1]), string.lower(arg) ) then
-                toreturn = value[1].."\n"..value[2]
+                toreturn = toreturn.."\n"..value[1].."\n"..value[2]
             end
         end
     return toreturn
